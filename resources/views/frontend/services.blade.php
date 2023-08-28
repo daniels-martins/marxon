09e2aac76f8e32eae258fe484c3732cf5225e6f2
Martins Daniels <danielsmrtns@gmail.com>
2023-08-28 09:23:56 -0400

    encapsulated the services_grid_section and replaced code in two files [DRY principle]

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -13,19 +13,7 @@
             </div>
             <div class="services-wrap bg-lighter br-10">
                 <div class="row no-gap">
-                    @foreach ($services as $service)
-                        <div class="col-xl-3 col-md-6">
-                            <div class="service-item wow fadeInUp delay-0-8s">
-                                <div class="icon">
-                                    <img src="/{{ $service->icon }}" alt="Icon">
-                                </div>
-                                <h4>{{ $service->title }}</h4>
-                                <a href="services.html" class="read-more">Explore <i
-                                        class="fas fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div>
-                    @endforeach
-
+                    @include('partials/general/services_grid')
                 </div>
             </div>
         </div>
--- resources/views/partials/welcome/services_section.blade.php
+++ resources/views/partials/welcome/services_section.blade.php
@@ -78,18 +78,8 @@
                                  class="fas fa-long-arrow-alt-right"></i></a>
                      </div>
                  </div> --}}
-                 @foreach ($services as $service)
-                     <div class="col-xl-4 col-md-6">
-                         <div class="service-item wow fadeInUp delay-0-8s">
-                             <div class="icon">
-                                 <img src="/{{ $service->icon }}" alt="Icon">
-                             </div>
-                             <h4>{{ $service->title }} </h4>
-                             <a href="{{ route('services') }}" class="read-more">Explore <i
-                                     class="fas fa-long-arrow-alt-right"></i></a>
-                         </div>
-                     </div>
-                 @endforeach
+                {{-- services grid --}}
+                @include('partials/general/services_grid')
 
              </div>
          </div>
